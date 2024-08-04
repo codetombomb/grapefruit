7d358633d8ee4331f81e9801b99d8fbaa01d8715
Tomas Tobar <codetombomb@gmail.com>
2024-08-04 18:23:29 -0500

    refactor disable icon - adjust popup styles

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,25 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.disableIcon) {
+const updateIconState = ({ disable, sender }) => {
+  if (disable) {
     chrome.action.disable(sender.tab.id);
     chrome.action.setIcon({
       path: "../icons/disable-grapefruit-logo-32.png",
       tabId: sender.tab.id,
     });
-    sendResponse({ message: "Icon disabled" });
   } else {
     chrome.action.enable(sender.tab.id);
     chrome.action.setIcon({
       path: "../icons/grapefruit-logo-32.png",
       tabId: sender.tab.id,
     });
+  }
+};
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.disableIcon) {
+    updateIconState({ disable: true, sender });
+    sendResponse({ message: "Icon disabled" });
+  } else {
+    updateIconState({ disable: false, sender });
     sendResponse({ error: "Icon enabled" });
   }
 });
